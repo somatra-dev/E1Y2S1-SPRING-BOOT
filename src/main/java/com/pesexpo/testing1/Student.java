aa75ce1d068ebe28f34db959786f70baadfe7272
somatra-dev <somatrachey9527@gmail.com>
2026-02-20 18:06:25 +0700

    modified structure

---
+++ src/main/java/com/pesexpo/testing1/Student.java
@@ -0,0 +1,11 @@
+package com.pesexpo.testing1;
+
+public record Student(
+
+        String id,
+        String name,
+        String gender,
+        Integer age
+
+) {
+}
---
+++ src/main/java/com/pesexpo/testing1/StudentController.java
@@ -0,0 +1,15 @@
+package com.pesexpo.testing1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public Student getStudent() {
+        return new Student("001", "Matra", "Male", 20);
+    }
+}
